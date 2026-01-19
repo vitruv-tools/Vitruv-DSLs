11eb9e81fc31dda618d2a6b0a5d7325b055ee94b
Miriam Boß <boss@kit.edu>
2026-01-19 17:04:41 +0100

    fixed code smell with isEmpty() and wrote tests for the JavaFileGenerator class (which is not used in at least this project)

---
+++ common/src/test/java/tools/vitruv/dsls/common/JavaFileGeneratorTest.java
@@ -0,0 +1,58 @@
+package tools.vitruv.dsls.common;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.*;
+
+public class JavaFileGeneratorTest {
+
+    @Test
+    void testGenerateSimple(){
+            String result = JavaFileGenerator.generateClass("","test",null);
+            String expectedResult = "package test;\n\n";
+            assertEquals(expectedResult, result,"only package should be printed");
+
+            result = JavaFileGenerator.generateClass("implementation","test",null);
+            expectedResult = "package test;\n\nimplementation";
+            assertEquals(expectedResult, result,"implementation is not printed");
+    }
+
+    @Test
+    void testGenerateImportHelper(){
+        JavaImportHelper importHelper = new JavaImportHelper();
+        StringBuilder expectedResult= new StringBuilder("package test;\n\n");
+
+        assertEquals(expectedResult.toString(),generateClassWithImportHelper(importHelper),"Empty Import failed");
+
+        importHelper.typeRef(JavaFileGenerator.class);
+        expectedResult.append("import ").append(JavaFileGenerator.class.getName()).append(";\n");
+        assertEquals(expectedResult+"\n\n",generateClassWithImportHelper(importHelper),"normal Import failed");
+
+        importHelper.typeRef(JavaFileGenerator.class.getName());
+        assertEquals(expectedResult+"\n\n",generateClassWithImportHelper(importHelper),"duplicate import not deduplicated");
+
+        importHelper.staticRef(JavaFileGenerator.class, "test");
+        expectedResult.append("import static ").append(JavaFileGenerator.class.getName()).append(";\n");
+        assertEquals(expectedResult +"\n\n",generateClassWithImportHelper(importHelper),"Static Import failed");
+
+    }
+
+    private String generateClassWithImportHelper(JavaImportHelper importHelper){
+        return JavaFileGenerator.generateClass("","test",importHelper);
+    }
+
+    @Test
+    void testGetJavaFilePath(){
+        String expectedResult = "package"+JavaFileGenerator.FSA_SEPARATOR+"class.java";
+        String result = JavaFileGenerator.getJavaFilePath("package.class");
+        assertEquals(expectedResult,result, "Simple test failed");
+
+        GenericClassNameGenerator nameGenerator = new GenericClassNameGenerator("package","class");
+        assertEquals(expectedResult,JavaFileGenerator.getJavaFilePath(nameGenerator), "Name Generator does not work");
+
+        expectedResult = "package1"+JavaFileGenerator.FSA_SEPARATOR+"package2"+JavaFileGenerator.FSA_SEPARATOR+"class.java";
+        result = JavaFileGenerator.getJavaFilePath("package1.package2.class");
+        assertEquals(expectedResult,result, "several directories does not work");
+    }
+
+}
