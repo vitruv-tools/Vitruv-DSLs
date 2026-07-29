67b81fc0b70910d5762bba7550c0bdd79ba5af7d
Arne Lange <arne.lange@kit.edu>
2026-07-29 11:54:28 +0200

    Fix vscode extension TS build: node16 module resolution + skipLibCheck
    vscode-languageclient v10's exports map requires node16 resolution to
    resolve the /node subpath, and skipLibCheck avoids unrelated type
    errors in vscode-jsonrpc's bundled .d.ts files caused by a TS/library
    version mismatch.

--- vitruvocl/vitruvocl-vscode-extension/out/extension.js
+++ vitruvocl/vitruvocl-vscode-extension/out/extension.js
@@ -1,12 +1,45 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.activate = activate;
 exports.deactivate = deactivate;
-const vscode = require("vscode");
-const child_process = require("child_process");
-const util = require("util");
-const fs = require("fs");
-const path = require("path");
+const vscode = __importStar(require("vscode"));
+const child_process = __importStar(require("child_process"));
+const util = __importStar(require("util"));
+const fs = __importStar(require("fs"));
+const path = __importStar(require("path"));
 const node_1 = require("vscode-languageclient/node");
 const execFile = util.promisify(child_process.execFile);
 const constraintResults = new Map();
@@ -474,7 +507,7 @@ function extractConstraint(text, constraintName) {
     return constraint.trim() || null;
 }
 async function writeTempConstraint(constraint) {
-    const os = await Promise.resolve().then(() => require('os'));
+    const os = await import('os');
     const tempDir = os.tmpdir();
     const tempFile = path.join(tempDir, `vitruvocl-${Date.now()}.ocl`);
     fs.writeFileSync(tempFile, constraint, 'utf-8');
